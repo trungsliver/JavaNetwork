b3dde3e232365abf6ada6bfc4af22bc88e14bb85
trungsliver <buiductrung.cva@gmail.com>
2025-08-20 09:23:36 +0700

    port scanner

---
+++ src/main/java/socket/PortScanner.java
@@ -0,0 +1,28 @@
+package socket;
+
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URL;
+
+public class PortScanner {
+    public static void main(String[] args) {
+        checkPort("localhost");
+    }
+
+    public static void checkPort(String urlString){
+        int startPort = 1;
+        int endPort = 100000;
+
+        System.out.println("Checking port: " + urlString);
+        for (int port = startPort; port <= endPort; port++) {
+            try {
+                Socket socket = new Socket(urlString, port);
+                System.out.println("Port " + port + " is open on " + urlString);
+                socket.close();
+            }catch (Exception e){}
+        }
+        System.out.println("Port scan completed for " + urlString);
+    }
+}
+
+// Can use wireShark :))))
